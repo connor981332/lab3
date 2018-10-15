ff4bf34f7c3eb40fffc5ad2c79c46be639f7e9fa
Connor Bunch <connorbunch@outlook.com>
2018-10-15 01:11:29 -0600

    adding skeleton for comments

--- avatar.js
+++ avatar.js
@@ -3,20 +3,7 @@ angular.module('app', [])
   .directive('avatar', avatarDirective);
 
 function mainCtrl ($scope) {
-  /*
-  Let's just make sure something happens when the user submits the form by binding the declared 'addNew' function to the scope.
-  You can see we are expecting a user object as a parameter. This is 'userForm'
-  */
-  // $scope.addNew = function (user) {
-  //   alert(user.name + ' ' + user.url);
-  // };
-  // tada! a users array will now be bound to and made available to our html template.
   $scope.users = [];
-  /*
-  1. We push a new 'user' object to our users list with a name and avatarUrl property
-  2. For our purposes, a quick and dirty method for clearing the form will do.
-  Just note there is a more appropriate method when form validation is involved.
-  */
   $scope.addNew = function (user) {
     $scope.users.push({
       name: user.name,
